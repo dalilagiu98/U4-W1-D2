56f05b5d003a58b4ccd8ef74dcd0f8d98ce7eb18
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-03-05 16:07:12 +0100

    exercise3

--- untitled/src/exercise3/Exercise3.java
+++ untitled/src/exercise3/Exercise3.java
@@ -2,18 +2,31 @@ package exercise3;
 
 import java.util.Objects;
 import java.util.Scanner;
-
 public class Exercise3 {
+
+    private static String separaCaratteri(String inputStringa) {
+        if (inputStringa.isEmpty()) {
+            return ""; // Restituisce una stringa vuota se l'input è vuoto
+        }
+
+        // Utilizza String.join per separare i caratteri con virgola
+        return String.join(",", inputStringa.split(""));
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String inputStringa = "";
 
-        System.out.println("Insert a string");
-        String input = scanner.nextLine();
+        while (!inputStringa.equals(":q")) { //finchè l'input è diverso da zero
+            System.out.println("Insert a string (or insert \":q\" to exit): ");
+            inputStringa = scanner.nextLine();
+            System.out.println("The characters of string are: " + separaCaratteri(inputStringa));
 
-        while(input.equals(":q")){
-            String[] separatedChar = input.split(",");
         }
 
+        System.out.println("Uscita dal programma.");
+
+        scanner.close();
     }
-}
+
+    }
